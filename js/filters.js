d1211d0d14e2246accaab35568abbad8f3b1035d
shafley <shafleyxia@gmail.com>
2015-05-29 23:23:00 +0800

    add filter

--- js/filters.js
+++ js/filters.js
@@ -3,3 +3,14 @@
 /* Filters */
 
 angular.module('music.filters', []);
+
+music.filter('offset', function () {
+    return function (input, start) {
+        if (input) {
+            start = parseInt(start, 10);
+            return input.slice(start);
+        } else {
+            return [];
+        }
+    };
+});
