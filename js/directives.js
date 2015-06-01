bc3b5aaa345e8fc8b771d730b16f3cf5e8f90a67
shafley <shafleyxia@gmail.com>
2015-06-01 23:12:28 +0800

    complete directive of pagination

--- js/directives.js
+++ js/directives.js
@@ -18,8 +18,45 @@ angular.module('music.directives', []);
 music.directive('pagination', function (){
     return {
         restrict: 'AE',
-        link:function (){
-
+        replace: true,
+        template: '\
+            <div>\
+                <span ng-click="prevPage()">上一页</span>\
+                <span ng-click="nextPage()">下一页</span>\
+            </div>',
+        link:function (scope, element, attrs){
+            // console.log(scope);
+            // console.log(attrs);
+            // attrs.list = scope.$eval(attrs.list);
+            // console.log("attrs.list: "+attrs.list.length);
+            // console.log("attrs.itemsPerPage: "+attrs.itemsPerPage);
+            scope.pageCount = function () {
+                if (attrs.list) {
+                    return Math.ceil(attrs.list.length / attrs.itemsPerPage);
+                } else {
+                    return 1;
+                }
+            };
+            scope.prevPage = function () {
+                if (attrs.currentPage > 0) {
+                    attrs.currentPage--;
+                    scope.currentPage = attrs.currentPage;
+                    console.log("上一页"+attrs.currentPage);
+                }
+                console.log("scope.currentPage："+scope.currentPage);
+                console.log("scope.itemsPerPage；"+scope.itemsPerPage);
+            };
+            scope.nextPage = function () {
+                if (attrs.currentPage < scope.pageCount()) {
+                    attrs.currentPage++;
+                    scope.currentPage = attrs.currentPage;
+                    console.log("下一页"+attrs.currentPage);
+                }
+                console.log("scope.currentPage："+scope.currentPage);
+                console.log("scope.itemsPerPage；"+scope.itemsPerPage);
+            };
+            scope.currentPage = attrs.currentPage;
+            scope.itemsPerPage = attrs.itemsPerPage;
         }
     }
 });
--- js/filters.js
+++ js/filters.js
@@ -6,6 +6,8 @@ angular.module('music.filters', []);
 
 music.filter('offset', function () {
     return function (input, start) {
+        // console.log("input: "+input);
+        // console.log("start: "+start);
         if (input) {
             start = parseInt(start, 10);
             return input.slice(start);
